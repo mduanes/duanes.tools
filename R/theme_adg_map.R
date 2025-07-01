2357f720bc0500360023a059d0767a11766dabdf
Micah Shannon <mds64585@uga.edu>
2025-07-01 13:10:17 -0400

    add themes

---
+++ R/theme_adg_map.R
@@ -0,0 +1,14 @@
+#' @name theme_adg_map
+#'
+#' @export
+#'
+#'
+#'
+theme_adg_map <- theme_minimal() +
+  theme(axis.text = element_blank(),
+        plot.background = element_rect(fill="white",linewidth = 0),
+        panel.grid = element_blank(),
+        legend.text = element_text(size=default_axis_text_size*0.75),
+        legend.title = element_text(size=default_axis_text_size,face="bold"),
+        plot.caption = element_text(size=0.6*default_axis_text_size,face="italic",
+                                    hjust=0.5))
