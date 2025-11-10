74520f39cc0425ab0e5820ece420595768465e8d
Micah Shannon <mds64585@uga.edu>
2025-11-10 15:21:57 -0500

    comp o change fix

--- R/get_components_of_change.R
+++ R/get_components_of_change.R
@@ -4,17 +4,26 @@
 #'
 #` pull population change components for given years
 
-pull_components <- function(adg_path=Sys.getenv("ADG_KEY")) {
+pull_components <- function(adg_path=Sys.getenv("ADG_KEY"),
+                            ctys=NA) {
 
+  if(is.na(ctys)) {
+    ctys <- us_counties() %>%
+      filter(STATEFP=="13") %>%
+      pull(NAME)
+  }
   if(Sys.getenv("ADG_KEY") == "") {
     print("No ADG Path! Set using install_adg_key()")
   } else {
 
     # fetch based on vintage
+    print(ctys)
     full_path <- paste0(adg_path,"Data/Census Population Estimates (All Components)/ga_components_of_change_10_24.csv")
 
     # read in base data and summarize by preferences
-    read_csv(full_path)
+    read_csv(full_path) %>%
+      filter(GEOID %in% get_fips(ctys)) %>%
+      mutate(CTYNAME =str_remove(CTYNAME," County"))
   }
 }
 
